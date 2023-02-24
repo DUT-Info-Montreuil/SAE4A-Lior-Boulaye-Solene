21b376a2210f0c9b6a3ba4fc791f512ce584dc11
Lior Lichten <llichten29@gmail.com>
2023-02-24 11:15:00 +0100

    Création des packages controleur et modèle et de la classe Utilisateur

--- code/app/src/main/java/com/example/enjoyyourmeal/controleur/MainActivity.java
+++ code/app/src/main/java/com/example/enjoyyourmeal/controleur/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.enjoyyourmeal;
+package com.example.enjoyyourmeal.controleur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.enjoyyourmeal.R;
 
 public class MainActivity extends AppCompatActivity {
 
