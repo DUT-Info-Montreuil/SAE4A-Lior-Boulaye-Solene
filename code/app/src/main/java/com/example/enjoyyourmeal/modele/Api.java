b7f5a8275021aeae6469b0ef12dd147c1083860e
Lior Lichten <llichten29@gmail.com>
2023-03-24 17:02:36 +0100

    Ajout de retrofit pour communiquer avec l'api

---
+++ code/app/src/main/java/com/example/enjoyyourmeal/modele/Api.java
@@ -0,0 +1,16 @@
+package com.example.enjoyyourmeal.modele;
+
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface Api {
+/*
+    public interface LoginService {
+        @FormUrlEncoded
+        @POST("/login.php")
+        void login(@Field("username") String username, @Field("motDePasse") String motDePasse);
+    }
+*/
+}
+
